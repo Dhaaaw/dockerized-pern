f2d0caf2ffb7ad2e1a14274694fa806aecc279fb
Dhaaaw <marnissidhia@yahoo.fr>
2021-05-28 00:00:29 +0100

    added a header, routes and a login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
 
 import Login from "./components/login";
 import Register from "./components/register";
@@ -11,25 +11,91 @@ import OtherProfile from "./components/otherProfile";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  const logout = () => {
+    localStorage.removeItem("user");
+  };
+
+  const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+
+  useEffect(() => {
+    const user = getCurrentUser();
+
+    if (user) setCurrentUser(user);
+  }, []);
+
   return (
     <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link to={"/"} className="navbar-brand">
+          Dockerized PERN
+        </Link>
+        {currentUser && (
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/search"} className="nav-link">
+                Browse profiles
+              </Link>
+            </li>
+          </div>
+        )}
+
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                {currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={logout}>
+                Logout
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Register
+              </Link>
+            </li>
+          </div>
+        )}
+      </nav>
+
+      <div className="d-flex align-items-center justify-content-center">
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/profile" component={OwnProfile} />
+          <Route exact path="/profile/:id" component={OtherProfile} />
+          <Route exact path="/search" component={SearchProfiles} />
+        </Switch>
+        {currentUser ?
+        <Redirect to="/profile"/> 
+        : 
+        <Redirect to="/login"/> 
+        }
+      </div>
     </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
